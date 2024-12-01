5ed9e091e91768be3c643f362447352634ba7910
OEM Configuration (temporary user) <felipematuranaaraya7@gmail.com>
2024-11-30 21:46:43 -0300

    last commit

--- scripts/views/admin.js
+++ scripts/views/admin.js
@@ -1,12 +1,13 @@
 import { urlAPI } from "../variables.js"
 
-document.addEventListener('DOMContentLoaded', async ()=>{
-    const url = urlAPI + `/auth/dashboard`;
-    console.log(url)
-    const respuesta = await fetch(url, {
-        method: "GET",
-        credentials: 'include'
-    })
-    const resultado = await respuesta.json();
-    console.log(resultado)
+const url = urlAPI + `/auth/dashboard`;
+console.log(url)
+const respuesta = await fetch(url, {
+    method: "GET",
+    credentials: 'include'
 })
+const resultado = await respuesta.json();
+
+if(resultado.error){
+    window.location.href = '/'
+}
